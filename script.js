484d0d2badebe55dac816cbfc4cab97e2d640144
FrancimarioAraujo <francimariofilho7@gmail.com>
2020-03-28 13:29:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var imgLamp = document.getElementById('lamp');
+
+function turnOn(){
+    imgLamp.setAttribute('src','img/lamp-on.jpg');
+}
+
+function turnOff(){
+    imgLamp.setAttribute('src','img/lamp.jpg');
+}
+
+
